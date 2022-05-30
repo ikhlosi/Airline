36bef5399429b6c376da050fa9210ff375902d46
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2022-05-30 17:37:26 +0200

    initial commit

---
+++ AirlineBL/Flight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBL {
+    internal class Flight {
+        private int _flightNum;
+        private DateTime _dateOfFlight;
+        private Route _route;
+        private int _seatsSold;
+        private Aircraft _airplane;
+    }
+}
